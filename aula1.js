e2e154e190195bf2a957ba380bda7e4a94cbefa8
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-02-24 08:21:12 -0300

    jdkck

---
+++ aula1.js
@@ -0,0 +1,29 @@
+/*Em java script têm três tipos de dados, o booleano, o numerico e a string. 
+O numerico é chamado de number
+Os dados são armazenados em costantes ou variaveis.*/
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+
+const segundoNumero = 2;
+
+const operacao = primeiroNumero+segundoNumero
+
+console.log (operacao)
+
+/*Pontos flutuantes são números com vírgula.*/
+
+const numerodecimal = 2.6
+
+const pontoflutuantesemzero = 0.9
+
+const novaoperacao = numerodecimal/pontoflutuantesemzero
+
+console.log (novaoperacao)
+
+const colegio = "mario braga"
+
+console.log (colegio*primeiroNumero)
+
+/*O NaN aparece quando você tenta fazer uma operaçâo matematica com uma string.*/
