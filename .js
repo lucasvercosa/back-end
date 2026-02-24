5b74343439c2df3ae403bf0cec6689113b9e8547
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-02-24 07:43:35 -0300

    kkkk

---
+++ .js
@@ -0,0 +1,15 @@
+/*Em java script têm três tipos de dados, o booleano, o numerico e a string. 
+O numerico é chamado de number
+Os dados são armazenados em costantes ou variaveis.*/
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+
+const segundoNumero = 2;
+
+const operacao = primeiroNumero+segundoNumero
+
+console.long (operacao)
+
+node aula1
